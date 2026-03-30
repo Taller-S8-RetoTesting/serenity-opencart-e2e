c7b966343aac00ea136320512e7eb97865840b62
AlexRieger47 <alexrieger.147@gmail.com>
2026-03-30 13:37:44 -0300

    feat: add initial project structure with Gradle configuration and Serenity setup

---
+++ src/test/java/org/alexrieger/util/TestData.java
@@ -0,0 +1,10 @@
+package org.alexrieger.util;
+
+public final class TestData {
+
+    // Insertar aquí las constantes de datos de prueba, por ejemplo:
+    // public static final String YAHOO_URL = "https://espanol.yahoo.com/";
+
+    private TestData() {
+    }
+}
